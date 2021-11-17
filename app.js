35b997d68140dc398ff9522a3dec4cc846467f99
Moiler <32693070+moiler@users.noreply.github.com>
2021-11-17 18:56:54 +1100

    Updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const client = new Client({
 });
 
 client.on('ready', () => {
-  console.log('Logged in as ' + client.user.tag);
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 const startBot = () => {
@@ -16,7 +16,7 @@ const startBot = () => {
   try {
     client.login(config.DISCORD_TOKEN);
   } catch (error) {
-    console.error("Error starting bot", { error })
+    console.error('Error starting bot', { error });
   }
 };
 
@@ -36,7 +36,7 @@ client.on('messageCreate', (message) => {
   if (command === commands.getName) {
     message.reply(message.author.username);
   } else if (command === commands.beep) {
-    play(message, "https://www.soundboard.com/mediafiles/mt/MTQ1MzI4MzAzMTQ1Mzgw_jwPFPnna9_2bs.mp3", 'beep boop beep');
+    play(message, "https://www.soundboard.com/mediafiles/mt/MTQ1MzI4MzAzMTQ1Mzgw_jwPFPnna9_2bs.mp3", 'beep boop beep =/');
     //message.reply('beep boop beep');
   } else if (command === commands.boop) {
     message.reply('boop beep boop');
